af6c7fe884143393115a1a45f8857ee446790004
raushan k <raushan.br.in@gmail.com>
2024-12-16 18:06:41 +0100

    uploaded file extraction

---
+++ config.py
@@ -0,0 +1,4 @@
+
+# Set up upload folder and templates
+UPLOAD_FOLDER = "uploads"
+INPUT_FILE_FORMAT = ".pdf"
--- main.py
+++ main.py
@@ -1,24 +1,28 @@
+import os
+from typing import List
+
 from fastapi import FastAPI, File, UploadFile
+from fastapi.requests import Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.requests import Request
-from typing import List
-import os
+
+from config import INPUT_FILE_FORMAT, UPLOAD_FOLDER
+from operations import process_uploaded_docs
 
 app = FastAPI(title="d-chat")
 
-# Set up upload folder and templates
-UPLOAD_FOLDER = 'pdf_uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 templates = Jinja2Templates(directory="templates")
 
 # Mount static files for serving CSS
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
 @app.get("/", response_class=HTMLResponse)
 async def landing_page(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
 
 @app.post("/upload")
 async def upload_files(files: List[UploadFile] = File(...)):
@@ -26,15 +30,18 @@ async def upload_files(files: List[UploadFile] = File(...)):
         return {"error": "No files selected"}
 
     for file in files:
-        if file.filename.endswith('.pdf'):
+        if file.filename.endswith(INPUT_FILE_FORMAT):
             file_path = os.path.join(UPLOAD_FOLDER, file.filename)
             with open(file_path, "wb") as f:
                 f.write(await file.read())
         else:
-            return {"error": f"File {file.filename} is not a PDF"}
+            return {"error": f"File {file.filename} is not a {INPUT_FILE_FORMAT}"}
 
+    process_uploaded_docs(file_path, INPUT_FILE_FORMAT)
     return {"message": "Files uploaded successfully!"}
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
