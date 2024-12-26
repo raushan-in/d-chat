33a6eaa6eee6503f26863c914882d5b7d708fe0b
raushan k <raushan.br.in@gmail.com>
2024-12-26 20:25:23 +0100

    config updated

--- config.py
+++ config.py
@@ -1,23 +1,31 @@
+"""
+Config Mnagment for the project
+"""
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # APP
 PORT = 8000
 
 # Set up upload folder and templates
-UPLOAD_FOLDER = "uploads"
-VECTOR_FOLDER = "vectors"
-INPUT_FILE_FORMAT = ".pdf"
+UPLOAD_FOLDER = os.environ.get("UPLOAD_FOLDER")
+VECTOR_FOLDER = os.environ.get("VECTOR_FOLDER")
+INPUT_FILE_FORMAT = os.environ.get("INPUT_FILE_FORMAT")
 
 
 # LLM
-LLM_CHECKPOINT_ID = "fine-tune/trained_models/flan-t5-base-fine-tuned" #"google/flan-t5-base"
-LLM_TASK = "text2text-generation"  # "text-generation", "summarization"
-LLM_TEMPERATURE = 0.4
+LLM_CHECKPOINT_ID = os.environ.get("LLM_CHECKPOINT_ID")
+LLM_TASK = os.environ.get("LLM_TASK")
+LLM_TEMPERATURE = float(os.environ.get("LLM_TEMPERATURE", "0.5"))
 
 # data embedding
-EMBADDING_MODEL = "fine-tune/trained_models/all-mpnet-base-v2-fine-tuned" #"sentence-transformers/all-mpnet-base-v2"
+EMBADDING_MODEL = os.environ.get("EMBADDING_MODEL")
 
 
 # Instructions for the system prompt
-BOT_NAME = "d-chat"
+BOT_NAME = os.environ.get("BOT_NAME", "d-bot")
 PROMPT_INSTRUCTIONS = [
     "Do not use any external resources to answer the question.",
     "Do not provide any personal information.",
